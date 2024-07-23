e20273cf70154533a1685c56cf1f43caa0ef5c7c
Noa Aarts <itepastra@gmail.com>
2024-07-23 22:33:06 +0200

    feat: add devshell, improve flake

--- flake.nix
+++ flake.nix
@@ -5,22 +5,42 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, ... }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
 
-    packages.x86_64-linux.default =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "automapaper";
-        src = ./.;
-        nativeBuildInputs = with nixpkgs; [ meson ninja pkg-config pacman libarchive ];
-        buildInputs = with nixpkgs; [ wayland libGL ];
-        configurePhase = "meson setup build";
-        buildPhase = "ninja -C build";
-        installPhase = ''
-          mkdir -p "$out/bin"
-          mv build/automapaper "$out/bin"
-        '';
-      };
-  };
+      packages = forAllSystems ({ system, pkgs, ... }:
+        pkgs.stdenv.mkDerivation {
+          name = "automapaper";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ meson ninja pkg-config pacman libarchive ];
+          buildInputs = with pkgs; [ wayland libGL ];
+          configurePhase = "meson setup build";
+          buildPhase = "ninja -C build";
+          installPhase = ''
+            mkdir -p "$out/bin"
+            mv build/automapaper "$out/bin"
+          '';
+        });
+
+      devShell = forAllSystems ({ system, pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            meson
+            ninja
+          ];
+        });
+    };
 }
 
