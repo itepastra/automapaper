e3da15a36171dcca26b05a04f9e6bd1d52f410a3
Noa Aarts <itepastra@gmail.com>
2024-07-23 22:21:57 +0200

    chore: format

--- flake.nix
+++ flake.nix
@@ -1,26 +1,26 @@
 {
-  description = "A very basic flake";
+  description = "A wallpaper that does automatons";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
   outputs = { nixpkgs, ... }: {
-    
-    packages.x86_64-linux.default = 
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-      name = "automapaper";
-      src = ./.;
-      nativeBuildInputs = with nixpkgs; [ meson ninja pkg-config pacman libarchive ];
-      buildInputs = with nixpkgs; [ wayland libGL ];
-      configurePhase = "meson setup build";
-      buildPhase = "ninja -C build";
-      installPhase = ''
-        mkdir -p $out/bin
-        mv build/automapaper $out/bin
-      '';
-    };
+
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "automapaper";
+        src = ./.;
+        nativeBuildInputs = with nixpkgs; [ meson ninja pkg-config pacman libarchive ];
+        buildInputs = with nixpkgs; [ wayland libGL ];
+        configurePhase = "meson setup build";
+        buildPhase = "ninja -C build";
+        installPhase = ''
+          mkdir -p "$out/bin"
+          mv build/automapaper "$out/bin"
+        '';
+      };
   };
 }
 
